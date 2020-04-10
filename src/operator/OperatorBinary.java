74cca3bf7a4e36e2397b5e2402a01a1a720e43fd
Ermawatii <62318461+Ermawatii@users.noreply.github.com>
2020-04-10 12:48:50 +0700

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -1,7 +1,17 @@
 package operator;
 
 public class OperatorBinary {
-    String nama = "Ermawati";
-    int jumlahSks = 24;
-    double indeksPrestasiKumulatif = 3.89;
+
+        String nama = "Ermawati";
+        int jumlahSks = 24;
+        double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
